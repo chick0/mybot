f653d3fe2122e1b70ca6f539c1eca6f6c92c5018
chick_0 <chick_0@ch1ck.xyz>
2021-05-04 02:09:42 +0900

    일일 선물 기능 추가

---
+++ commands/gift.py
@@ -0,0 +1,61 @@
+from random import randint
+from datetime import datetime, timedelta
+
+from discord.ext import commands
+from sqlalchemy.orm import sessionmaker
+
+from database import engine
+from models import Point, Gift
+
+
+class Cog(commands.Cog, name="선물 상자"):
+    @commands.command(help="24시간 마다")
+    async def daily(self, ctx: commands.context):
+        async def gift_():
+            point = session.query(Point).filter_by(
+                owner=ctx.author.id
+            ).first()
+            if point is None:
+                point = Point()
+                point.owner = ctx.author.id
+                point.point = 0
+                session.add(point)
+
+            gift_point = randint(300, 800)
+            point.point += gift_point
+            session.commit()
+
+            await ctx.reply(
+                f"선물 상자에서 {gift_point}P 를 받았습니다!"
+            )
+
+        gift_code = "24h"
+
+        session_ = sessionmaker(bind=engine.get_engine())
+        session = session_()
+
+        gift_stat = session.query(Gift).filter_by(
+            owner=ctx.author.id,
+            type=gift_code
+        ).first()
+        if gift_stat is None:
+            gift_stat = Gift()
+            gift_stat.owner = ctx.author.id
+            gift_stat.type = gift_code
+            gift_stat.date = datetime.now() + timedelta(hours=24)
+
+            session.add(gift_stat)
+            session.commit()
+            await gift_()
+        else:
+            if gift_stat.date <= datetime.now():
+                gift_stat.date = datetime.now() + timedelta(hours=24)
+                session.commit()
+                await gift_()
+            else:
+                await ctx.reply(
+                    "아직 선물 상자가 도착하지 않았습니다"
+                )
+
+
+
