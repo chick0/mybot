e39db1904935385222aeb0706fc3329c9a5767ec
chick_0 <chick_0@ch1ck.xyz>
2021-05-04 21:06:47 +0900

    선물 기능 구조 변경

--- commands/gift.py
+++ commands/gift.py
@@ -8,27 +8,28 @@ from database import engine
 from models import Point, Gift
 
 
+async def gift(session, ctx: commands.context, a: int, b: int):
+    point = session.query(Point).filter_by(
+        owner=ctx.author.id
+    ).first()
+    if point is None:
+        point = Point()
+        point.owner = ctx.author.id
+        point.point = 0
+        session.add(point)
+
+    gift_point = randint(a, b)
+    point.point += gift_point
+    session.commit()
+
+    await ctx.reply(
+        f"선물 상자에서 {gift_point}P 를 받았습니다!"
+    )
+
+
 class Cog(commands.Cog, name="선물 상자"):
     @commands.command(help="24시간 마다")
     async def daily(self, ctx: commands.context):
-        async def gift_():
-            point = session.query(Point).filter_by(
-                owner=ctx.author.id
-            ).first()
-            if point is None:
-                point = Point()
-                point.owner = ctx.author.id
-                point.point = 0
-                session.add(point)
-
-            gift_point = randint(300, 800)
-            point.point += gift_point
-            session.commit()
-
-            await ctx.reply(
-                f"선물 상자에서 {gift_point}P 를 받았습니다!"
-            )
-
         gift_code = "24h"
 
         session_ = sessionmaker(bind=engine.get_engine())
@@ -46,12 +47,16 @@ class Cog(commands.Cog, name="선물 상자"):
 
             session.add(gift_stat)
             session.commit()
-            await gift_()
+            await gift(
+                session, ctx, a=300, b=500
+            )
         else:
             if gift_stat.date <= datetime.now():
                 gift_stat.date = datetime.now() + timedelta(hours=24)
                 session.commit()
-                await gift_()
+                await gift(
+                    session, ctx, a=300, b=500
+                )
             else:
                 await ctx.reply(
                     "아직 선물 상자가 도착하지 않았습니다"
