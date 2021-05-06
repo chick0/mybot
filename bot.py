29a415611525510be592775e382476b94aae01d0
chick_0 <chick_0@ch1ck.xyz>
2021-05-06 20:11:27 +0900

    명령어 사용 로그 기록

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-from sys import exit
+from sys import exit, version_info as py_ver
+from logging import INFO, getLogger, Formatter
+from logging import StreamHandler, FileHandler
 
 from discord import Status
+from discord import version_info as dpy_ver
 from discord.ext.commands import AutoShardedBot
 
 
@@ -9,16 +12,28 @@ bot = AutoShardedBot(
     command_prefix=";"
 )
 
-
 if __name__ == "__main__":
+    logger = getLogger()
+    logger.setLevel(INFO)
+    fmt = Formatter("%(asctime)s [%(levelname)s]: %(message)s", "%Y-%m-%d %H:%M:%S")
+    s_handle, f_handle = StreamHandler(), FileHandler("bot.log")
+    s_handle.setFormatter(fmt), f_handle.setFormatter(fmt)
+    logger.addHandler(s_handle), logger.addHandler(f_handle)
+    logger.info(
+        "Starting discord bot / using"
+        f" py({ py_ver.major}.{ py_ver.minor}.{ py_ver.micro}), "
+        f"dpy({dpy_ver.major}.{dpy_ver.minor}.{dpy_ver.micro})"
+    )
+
     try:
         with open("token.txt", mode="r") as tk:
             token = tk.readline()
     except FileNotFoundError:
-        print("token undefined")
+        logger.critical("token undefined")
         exit(-1)
 
     import commands
+
     for command in commands.__all__:
         module = getattr(commands, command)
         bot.add_cog(getattr(module, "Cog")(bot))
@@ -28,6 +43,18 @@ if __name__ == "__main__":
             status=Status.dnd
         )
 
+    async def on_command(ctx):
+        logger.info(f"({ctx.author.id}){ctx.author}: {ctx.message.content}")
+
+
+    async def on_command_error(ctx, error):
+        if error.__class__.__name__ in ["CommandNotFound", "CheckFailure", "NotOwner"]:
+            return
+
+        if error.__class__.__name__ == "MissingRequiredArgument":
+            return await ctx.reply("명령어에 필요한 옵션이 입력되지 않았습니다")
+
     bot.add_listener(on_ready, "on_ready")
+    bot.add_listener(on_command, "on_command")
+    bot.add_listener(on_command_error, "on_command_error")
     bot.run(token)
-    del token
